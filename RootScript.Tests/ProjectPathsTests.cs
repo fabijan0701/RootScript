b2257f2c927945c7209a5958b972198a73e6f3c7
Fabijan Šarić <fsaric@pmfst.hr>
2025-06-08 01:54:51 +0200

    Initial commit

---
+++ RootScript.Tests/ProjectPathsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace RootScript.Tests;
+
+public class ProjectPathsTests
+{
+    [Test]
+    public void ProjectRootPathExistsTest()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(Directory.Exists(Project.SolutionPath));
+            Assert.That(Directory.Exists(Project.RootScript));
+            Assert.That(Directory.Exists(Project.RootScriptTests));
+        });
+    }
+}
---
+++ RootScript/Project.cs
@@ -0,0 +1,16 @@
+﻿namespace RootScript;
+
+public class Project
+{
+    private static string GetPath()
+    {
+        // get path to the current directory
+        var pathParts = Directory.GetCurrentDirectory().Split('\\').ToList();
+        pathParts.RemoveRange(pathParts.Count - 4, 4);
+        return string.Join('\\', pathParts);
+    }
+
+    public static string SolutionPath => GetPath();
+    public static string RootScript => $"{SolutionPath}\\RootScript";
+    public static string RootScriptTests => $"{SolutionPath}\\RootScript.Tests";
+}
